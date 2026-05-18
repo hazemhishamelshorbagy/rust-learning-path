8077fe6078b935e8dfd1063a93fdfb258ecdd9e7
hazemhishamelshorbagy <hazem.frontend@gmail.com>
2026-05-18 12:58:21 +0200

    chapter 4

---
+++ create_bank_account/target/debug/deps/create_bank_account-cd54e549066fb495.d
@@ -0,0 +1,5 @@
+D:\rust-learning-path\create_bank_account\target\debug\deps\create_bank_account-cd54e549066fb495.d: src\main.rs
+
+D:\rust-learning-path\create_bank_account\target\debug\deps\libcreate_bank_account-cd54e549066fb495.rmeta: src\main.rs
+
+src\main.rs:
